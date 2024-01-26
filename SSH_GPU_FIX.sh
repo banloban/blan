c998454a14b1f539aa23213316f30e53730312ba
banloban <157822020+banloban@users.noreply.github.com>
2024-01-26 18:35:57 +0100

    Update SSH_GPU_FIX.sh

--- SSH_GPU_FIX.sh
+++ SSH_GPU_FIX.sh
@@ -19,7 +19,7 @@ echo Updating Please Wait
 echo "======================="
 sudo apt update > /dev/null 2>&1
 echo "==========openssh============="
-sudo apt install openssh-server > /dev/null 2>&1
+sudo apt install openssh-server
 echo "==========ED openssh============="
 mkdir -p /var/run/sshd
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
@@ -29,7 +29,7 @@ echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 echo "==========start openssh============="
 sudo service ssh start
 echo "===================================="
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo create root password
 passwd
 echo "===================================="
