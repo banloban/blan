04a10c6e28b463a5f54a83a4b9286a58d1933720
banloban <157822020+banloban@users.noreply.github.com>
2024-01-26 17:03:33 +0100

    Update GCngrok.sh

--- GCngrok.sh
+++ GCngrok.sh
@@ -5,4 +5,4 @@ echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 CRP = "2bUrmDJEZimULQAPbmQCmzpL2AQ_3kZk9AwKZgCVcYFT4Ld1p"
-./ngrok authtoken $CRP 
+./ngrok authtoken "2bUrmDJEZimULQAPbmQCmzpL2AQ_3kZk9AwKZgCVcYFT4Ld1p" 
